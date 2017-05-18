f1ab8547b3f9e771d372e2bedf539254517bfeb8
felit <nofelit@126.com>
2017-05-18 17:49:25 +0800

    添加lvs

--- load-balance/lvs/fabfile.py
+++ load-balance/lvs/fabfile.py
@@ -3,6 +3,12 @@ from __future__ import with_statement
 from fabric.api import run, env, execute, roles, runs_once, parallel, sudo, hosts, cd, task
 from fabric.contrib import files
 
+env.hosts = ['dev@192.168.181.21',
+             'dev@192.168.181.22',
+             'dev@192.168.181.23',
+             'dev@192.168.181.24']
 
+
+@task
 def load():
-    sudo('yum install ipvsadm')
+    sudo('yum install -y ipvsadm')
