46a9629055d9a43ce5496361890a38e9dab51a51
felit <nofelit@126.com>
2017-06-05 14:43:44 +0800

    添加fabfile.py

--- fabfile.py
+++ fabfile.py
@@ -3,6 +3,8 @@ from __future__ import with_statement
 from fabric.api import run, env, execute, roles, runs_once, parallel, sudo, hosts, cd, local
 from fabric.contrib import files
 
+env.hosts = ['root@139.198.6.107']
+
 
 def mount_deploy_disk():
     if not files.exists('/data'):
