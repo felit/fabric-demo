d80d9e31941c0acdc5000a5392024db67776b48c
felit <nofelit@126.com>
2017-06-07 00:26:51 +0800

    添加文件fabfile

--- load-balance/lvs/surealived/fabfile.py
+++ load-balance/lvs/surealived/fabfile.py
@@ -3,9 +3,9 @@ from __future__ import with_statement
 from fabric.api import run, env, execute, roles, runs_once, parallel, sudo, hosts, cd, task
 from fabric.contrib import files
 
-env.hosts = ['root@139.198.6.107']
+env.hosts = []
 
 
 @task
 def load():
-    sudo('yum install -y ipvsadm')
+    pass
