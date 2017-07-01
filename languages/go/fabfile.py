4b4fd87bef16d0a1f71031eb5537f4c5ca7f5ee5
felit <nofelit@126.com>
2017-07-02 00:12:36 +0800

    添加go语言、jdk安装

---
+++ languages/go/fabfile.py
@@ -0,0 +1,33 @@
+# -*- coding:utf8 -*-
+# 安装
+"""
+
+"""
+
+from __future__ import with_statement
+from fabric.api import run, env, task
+from fabric.contrib import files
+
+env.hosts = [
+    'vagrant@192.168.18.164',
+    'vagrant@192.168.18.190'
+]
+
+
+def test_install():
+    install()
+
+
+@task
+def install(url='https://storage.googleapis.com/golang/go1.8.3.linux-amd64.tar.gz'):
+    run('wget %s -O go.tar.gz' % url)
+    run('tar -zxvf go.tar.gz')
+    dir = run("tar -tf go.tar.gz | awk -F/ '{print $1}' | tail -n 1")
+    run('mv %s /opt/' % dir)
+    files.append('.bashrc', 'export GOROOT=/opt/go')
+    files.append('.bashrc', 'export GOBIN=$GOROOT/bin')
+    files.append('.bashrc', 'export GOPKG=$GOROOT/pkg/tool/linux_amd64')
+    files.append('.bashrc', 'export GOARCH=amd64')
+    files.append('.bashrc', 'export GOOS=linux')
+    files.append('.bashrc', 'export GOPATH=$GOROOT')
+    files.append('.bashrc', 'export PATH=$PATH:$GOBIN:$GOPKG:$GOPATH/bin')
--- languages/java/jdk/fabfile.py
+++ languages/java/jdk/fabfile.py
@@ -1,11 +1,40 @@
-#-*- coding:utf8 -*-
+# -*- coding:utf8 -*-
 # 安装
 """
 TODO 查找文件
 解压文件
 
 """
-def install(path):
-    # 查看根目录
-    # tar -tf jdk-8u66-linux-x64.tar.gz | awk -F "/" '{print $1}' | tail -n 1
-    pass
+
+from __future__ import with_statement
+from fabric.api import run, env, execute, roles, runs_once, parallel, sudo, hosts, cd, task, settings
+from fabric.contrib import files
+
+env.hosts = [
+    # 'vagrant@192.168.18.164',
+    'vagrant@192.168.18.190'
+]
+
+
+@task
+def test_install():
+    install('/home/vagrant')
+
+
+@task
+def install(path, install_path='/opt/jdk'):
+    # 查看根目录d
+    # 取第一个
+    """
+    TODO 如果查不到文件本地上传
+        :param path:
+        :param install_path:
+        :return:
+    """
+    with cd(path):
+        jdk_filename = run("ls -lh | grep jdk | tail -1 | awk '{print $9}'")
+    run('tar -zxvf %s' % jdk_filename)
+    dir = run("tar -tf %s | awk -F/ '{print $1}' | tail -n 1" % jdk_filename)
+    run('mv %s %s' % (dir, install_path))
+    files.append('.bashrc', 'export JAVA_HOME=%s' % install_path)
+    files.append('.bashrc', 'export PATH=$JAVA_HOME/bin:$PATH')
